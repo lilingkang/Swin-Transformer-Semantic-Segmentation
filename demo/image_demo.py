714e8caaae25d9cee56a8718e165310fa090ac0e
llk <872706089@qq.com>
2022-08-24 10:17:24 +0800

    main

--- demo/image_demo.py
+++ demo/image_demo.py
@@ -6,14 +6,14 @@ from mmseg.core.evaluation import get_palette
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('img', help='Image file')
-    parser.add_argument('config', help='Config file')
-    parser.add_argument('checkpoint', help='Checkpoint file')
+    parser.add_argument('--img', default='demo.png', help='Image file')
+    parser.add_argument('--config', default='Swin-Transformer-Semantic-Segmentation/configs/swin/upernet_swin_tiny_patch4_window7_512x512_160k_ade20k.py', help='Config file')
+    parser.add_argument('--checkpoint', default='Swin-Transformer-Semantic-Segmentation/upernet_swin_tiny_patch4_window7_512x512.pth', help='Checkpoint file')
     parser.add_argument(
         '--device', default='cuda:0', help='Device used for inference')
     parser.add_argument(
         '--palette',
-        default='cityscapes',
+        default='ade20k',
         help='Color palette used for segmentation map')
     args = parser.parse_args()
 
